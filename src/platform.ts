8af679c69daecad862be42e2fdc7cdaf4256b968
Mathieu Durand <2133200@cegeplimoilou.ca>
2022-10-01 18:59:27 -0400

    .

--- src/platform.ts
+++ src/platform.ts
@@ -66,7 +66,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(config.serviceUUID);
+      const uuid = this.api.hap.uuid.generate(device.serviceUUID);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
